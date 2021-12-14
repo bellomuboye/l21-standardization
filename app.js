20fa977c008291b8a26fa468dabbe54d3bfe9fdf
Bell Ombuoye <62097741+bellomuboye@users.noreply.github.com>
2021-12-14 20:11:28 +0100

    file structure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+const app = express();
+
+const routes = require("./src/routes")
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/ping', (req, res) => {
+    res.status(200).send(
+        {status: "operational",
+        message: "view documentation on https://github.com/bellomuboye/l21-standardization#readme"})
+});
+
+app.use("/api", routes)
+
+module.exports = app;
+
